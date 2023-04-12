b4c499b1c74c9b08be061192c1e58d87b85a633f
Graham Noel <grahamnoel@gmail.com>
2023-04-12 17:45:30 +0100

    finish extraction of backstage
    - all specs passing again

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -19,6 +19,14 @@ class GildedRose
       item.quality += 1 if item.quality < 50
     end
 
+    if item.sell_in < 6
+      item.quality += 1 if item.quality < 50
+    end
+
+    if item.sell_in < 0
+      item.quality = 0
+    end
+
     item.sell_in -= 1
   end
 
