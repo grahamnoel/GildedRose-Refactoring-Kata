487be507485bab4d825bcc9bb9400e6003e282be
Graham Noel <grahamnoel@gmail.com>
2023-04-12 18:02:23 +0100

    extract update methods to classes

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -5,14 +5,69 @@ class GildedRose
   end
 
   def update_brie(item)
-    item.quality += 1 if item.quality < 50
-    item.sell_in -= 1
+    Brie.new(item).update
   end
 
   def update_sulfuras(item)
+    Sulfuras.new(item).update
   end
 
   def update_backstage(item)
+    Backstage.new(item).update
+  end
+
+  def update_standard(item)
+    Standard.new(item).update
+  end
+
+  def update_quality()
+    @items.each do |item|
+      case item.name
+      when "Aged Brie"
+        return update_brie(item)
+      when "Sulfuras, Hand of Ragnaros"
+        return update_sulfuras(item)
+      when "Backstage passes to a TAFKAL80ETC concert"
+        return update_backstage(item)
+      else
+        return update_standard(item)
+      end
+    end
+  end
+end
+
+class Brie
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    item.quality += 1 if item.quality < 50
+    item.sell_in -= 1
+  end
+end
+
+class Sulfuras
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+  end
+end
+
+class Backstage
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update
     item.quality += 1 if item.quality < 50
 
     if item.sell_in < 11
@@ -29,8 +84,16 @@ class GildedRose
 
     item.sell_in -= 1
   end
+end
 
-  def update_standard(item)
+class Standard
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update
     item.quality -= 1 if item.quality > 0
     
     if item.sell_in < 0
@@ -38,21 +101,6 @@ class GildedRose
     end
 
     item.sell_in -= 1
-  end
-
-  def update_quality()
-    @items.each do |item|
-      case item.name
-      when "Aged Brie"
-        return update_brie(item)
-      when "Sulfuras, Hand of Ragnaros"
-        return update_sulfuras(item)
-      when "Backstage passes to a TAFKAL80ETC concert"
-        return update_backstage(item)
-      else
-        return update_standard(item)
-      end
-    end
   end
 end
 
