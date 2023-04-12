c69d7a6b8b96168543ac7b22cfe05a9f2aa4b41f
Graham Noel <grahamnoel@gmail.com>
2023-04-12 16:48:18 +0100

    allow future specs to alter item details

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -3,6 +3,12 @@ require File.join(File.dirname(__FILE__), 'gilded_rose')
 describe GildedRose do
 
   describe "#update_quality" do
+
+    let(:item) { Item.new(name, sell_in, quality) }
+    let(:name) { "foo" }
+    let(:sell_in) { 10 }
+    let(:quality) { 9 }
+
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
@@ -10,13 +16,13 @@ describe GildedRose do
     end
 
     it "decrements sell_in by 1" do
-      items = [Item.new("foo", 10, 9)]
+      items = [item]
       GildedRose.new(items).update_quality()
       expect(items[0].sell_in).to eq 9
     end
 
     it "decrements quality by 1" do
-      items = [Item.new("foo", 10, 9)]
+      items = [item]
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 8
     end
