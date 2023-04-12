4d185eff99dd596f8dc6523b9be21e2650ea7712
Graham Noel <grahamnoel@gmail.com>
2023-04-12 18:20:19 +0100

    add new specs for "conjured"

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -212,5 +212,41 @@ describe GildedRose do
         end
       end
     end
+
+    context "conjured" do
+      let(:name) { "Conjured Mana Cake" }
+
+      it "decrements sell_in by 1" do
+        items = [item]
+        GildedRose.new(items).update_quality
+        expect(items[0].sell_in).to eq 9
+      end
+
+      it "decrements quality by 2" do
+        items = [item]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq 7
+      end
+
+      context "and quality at zero" do
+        let(:quality) { 0 }
+
+        it "does not go lower than zero" do
+          items = [item]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq 0
+        end
+      end
+
+      context "and quality at 1" do
+        let(:quality) { 1 }
+
+        it "does not go lower than zero" do
+          items = [item]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq 0
+        end
+      end
+    end
   end
 end
