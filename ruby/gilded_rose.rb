654f74b750dbc135f401fabffe890c6ab502356f
Graham Noel <grahamnoel@gmail.com>
2023-04-12 17:18:02 +0100

    trap brie, to break specs

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,6 +6,9 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
+      if item.name == "Aged Brie"
+        return
+      end
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
