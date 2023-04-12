286e8b64ca5da34f95f899df04450e9564c7c0f7
Graham Noel <grahamnoel@gmail.com>
2023-04-12 16:45:43 +0100

    implement first specs for standard item

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -8,6 +8,18 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+
+    it "decrements sell_in by 1" do
+      items = [Item.new("foo", 10, 9)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 9
+    end
+
+    it "decrements quality by 1" do
+      items = [Item.new("foo", 10, 9)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 8
+    end
   end
 
 end
