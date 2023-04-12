1bf0b5977ccd44b5442c7a5366079bde1ac27f95
Graham Noel <grahamnoel@gmail.com>
2023-04-12 16:59:24 +0100

    style change - remove parens

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -11,19 +11,19 @@ describe GildedRose do
 
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
+      GildedRose.new(items).update_quality
       expect(items[0].name).to eq "foo"
     end
 
     it "decrements sell_in by 1" do
       items = [item]
-      GildedRose.new(items).update_quality()
+      GildedRose.new(items).update_quality
       expect(items[0].sell_in).to eq 9
     end
 
     it "decrements quality by 1" do
       items = [item]
-      GildedRose.new(items).update_quality()
+      GildedRose.new(items).update_quality
       expect(items[0].quality).to eq 8
     end
 
@@ -32,13 +32,13 @@ describe GildedRose do
 
       it "decrements sell_in by 1" do
         items = [item]
-        GildedRose.new(items).update_quality()
+        GildedRose.new(items).update_quality
         expect(items[0].sell_in).to eq -2
       end
 
       it "decrements quality by 2" do
         items = [item]
-        GildedRose.new(items).update_quality()
+        GildedRose.new(items).update_quality
         expect(items[0].quality).to eq 7
       end
     end
@@ -48,7 +48,7 @@ describe GildedRose do
 
       it "does not go lower than zero" do
         items = [item]
-        GildedRose.new(items).update_quality()
+        GildedRose.new(items).update_quality
         expect(items[0].quality).to eq 0
       end
     end
@@ -58,13 +58,13 @@ describe GildedRose do
 
       it "decrements sell_in by 1" do
         items = [item]
-        GildedRose.new(items).update_quality()
+        GildedRose.new(items).update_quality
         expect(items[0].sell_in).to eq 9
       end
 
       it "increments quality by 1" do
         items = [item]
-        GildedRose.new(items).update_quality()
+        GildedRose.new(items).update_quality
         expect(items[0].quality).to eq 10
       end
 
@@ -73,11 +73,10 @@ describe GildedRose do
 
         it "does not go above 50" do
           items = [item]
-          GildedRose.new(items).update_quality()
+          GildedRose.new(items).update_quality
           expect(items[0].quality).to eq 50
         end
       end
     end
   end
-
 end
